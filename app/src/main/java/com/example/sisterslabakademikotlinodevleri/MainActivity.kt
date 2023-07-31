8256eeae4571e9e81008a164b154c22e2b6beb03
YUSUF\yusuf <yusufmendes123@gmail.com>
2023-07-31 14:44:12 +0300

    Add question one third week

--- app/src/main/java/com/example/sisterslabakademikotlinodevleri/MainActivity.kt
+++ app/src/main/java/com/example/sisterslabakademikotlinodevleri/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.sisterslabakademikotlinodevleri
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sisterslabakademikotlinodevleri.databinding.ActivityMainBinding
+import com.example.sisterslabakademikotlinodevleri.hafta3.odev1.UrunActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.urunTasariminaGitButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, UrunActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/sisterslabakademikotlinodevleri/hafta3/odev1/UrunActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sisterslabakademikotlinodevleri.hafta3.odev1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sisterslabakademikotlinodevleri.R
+
+class UrunActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_urun)
+    }
+}
